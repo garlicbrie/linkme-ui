c609f2d426413faa9576db35f22eb235561db526
Hannah Kim <hannah.kim@beanworks.com>
2019-05-19 20:52:42 -0700

    added storybook and created functional component for linkcards

---
+++ src/components/LinkCards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LinkCards = props => {
+    return (
+        <div>
+            HELLO!!!!!
+        </div>
+    );
+};
+
+LinkCards.propTypes = {
+    
+};
+
+export default LinkCards;
