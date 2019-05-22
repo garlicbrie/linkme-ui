60c59424478465434ebdf730c6df508a389ff3e8
Hannah Kim <hannah.kim@beanworks.com>
2019-05-21 19:11:00 -0700

    added basic content for linkcard

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {
+    Card as MaterialCard,
+    CardContent as MaterialCardContent,
+} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
+const CardContent = styled(MaterialCardContent)`
+    background-color: #EFF3F6;
+`;
 
 const LinkCard = props => {
     return (
         <div>
-            "HELLO"
+            <MaterialCard>
+                <CardContent>
+                    <Typography variant="h5" component="h2" gutterBottom>Python Tutorial for Beginners | Full Python Programming Course [2019]</Typography>
+                    <Typography component="p">This is where description would go wow amazing that’s so awsome this is a cool article</Typography>
+                    <Typography component="p">watched by user</Typography>
+                    <Typography component="p">added by user</Typography>
+                </CardContent>
+            </MaterialCard>
         </div>
     );
 };
